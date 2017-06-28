ddcae17b55f7d5d5202e31702866ac238892c91f
Anthony Lukach <alukach@cadasta.org>
2017-06-27 22:59:12 -0600

    Cleanup

--- worker-boilerplate/app/celery.py
+++ worker-boilerplate/app/celery.py
@@ -1,21 +1,24 @@
 from __future__ import absolute_import
 
 from celery import Celery
+from celery.signals import worker_init
+
+
+@worker_init.connect
+def configure_workers(sender=None, conf=None, **kwargs):
+    p = app.amqp.producer_pool.acquire()
+    try:
+        p.channel.exchange_declare(
+            exchange=app.conf.result_exchange, type=app.conf.result_exchange_type)
+        p.channel.queue_bind(
+            app.conf.PLATFORM_QUEUE_NAME, app.conf.result_exchange,
+            routing_key='#')
+        [p.maybe_declare(q) for q in app.conf.task_queues]
+    finally:
+        p.release()
 
 
 app = Celery('app',
              task_cls='app.celeryutils.Task',
              backend='app.celeryutils.ResultQueueRPC',)
 app.config_from_object('app.celeryconfig')
-
-p = app.amqp.producer_pool.acquire()
-try:
-    p.channel.exchange_declare(
-        exchange=app.conf.result_exchange, type=app.conf.result_exchange_type)
-    [p.maybe_declare(q) for q in app.conf.task_queues]
-finally:
-    p.release()
-
-
-if __name__ == '__main__':
-    app.start()
--- worker-boilerplate/app/celeryutils.py
+++ worker-boilerplate/app/celeryutils.py
@@ -25,13 +25,6 @@ class Task(_Task):
 
 class ResultQueueRPC(RPCBackend):
 
-    # TODO: Declare queues by returning them from below
-    # def on_reply_declare(self, task_id):
-    #     # Return value here is used as the `declare=` argument
-    #     # for Producer.publish.
-    #     # By default we don't have to declare anything when sending a result.
-    #     pass
-
     def _create_exchange(self, name, type='topic', delivery_mode=2):
         # Use app's provided exchange information rather than a RPCBackend's
         # default of an unnamed direct exchange
@@ -40,20 +33,13 @@ class ResultQueueRPC(RPCBackend):
     def store_result(self, *args, **kwargs):
         """
         Ensure that 'reply_to' queue is registered with exchange, so
-        that results are routed to both 'reply_to' queue and result
+        that results are routed to both 'reply_to' queue and platform
         queue.
         """
-
-        # TODO: This may be cleaner if we override
-        # kombu.transport.virtual.base.Channel.exchange_types with a new
-        # type of 'topic' class that always returns the routing_key when
-        # routing
         request = kwargs['request']
         reply_to = request.reply_to
-
-        with self.app.pool.acquire_channel(block=True) as (_, channel):
-            channel.queue_bind(
-                'platform.fifo', self.exchange.name, routing_key=reply_to)
-            channel.queue_bind(
-                reply_to, self.exchange.name, routing_key=reply_to)
-            return super(ResultQueueRPC, self).store_result(*args, **kwargs)
+        if reply_to:
+            with self.app.pool.acquire_channel(block=True) as (_, channel):
+                channel.queue_bind(
+                    reply_to, self.exchange.name, routing_key=reply_to)
+        return super(ResultQueueRPC, self).store_result(*args, **kwargs)
